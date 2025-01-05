04bc8419316f3fe39a4ceafa744ddba15a330d5c
JoneiSantos <jonei.santos@outlook.com.br>
2025-01-05 20:26:23 -0300

    add new routes - get users

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,12 +1,10 @@
 import { Request, Response } from 'express';
 import User from '../models/User';
+import mongoose from 'mongoose';
 
 export const registerUser = async (req: Request, res: Response): Promise<void> => {
   try {
-
     //console.log(`log kkkk>>> ${JSON.stringify(req.body)}`);
-
-
     const { name, email, password } = req.body;
 
     // Validação simples
@@ -32,6 +30,38 @@ export const registerUser = async (req: Request, res: Response): Promise<void> =
     });
   } catch (error) {
     console.error('Erro ao registrar usuário:', error);
+    res.status(500).json({ error: 'Erro interno no servidor' });
+  }
+};
+
+export const getAllUsers = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const users = await User.find().select('-password'); // Busca todos os usuários no banco de dados
+    res.status(200).json(users);
+  } catch (error) {
+    console.error('Erro ao listar usuários:', error);
+    res.status(500).json({ error: 'Erro interno no servidor' });
+  }
+};
+
+export const getUserById = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.isValidObjectId(id)) {
+      res.status(400).json({ error: 'ID inválido' });
+      return;
+    }
+
+    const user = await User.findById(id).select('-password');
+    if (!user) {
+      res.status(404).json({ error: 'Usuário não encontrado' });
+      return;
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error('Erro ao obter usuário:', error);
     res.status(500).json({ error: 'Erro interno no servidor' });
   }
 };
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { registerUser } from '../controllers/userController';
+import { getAllUsers, getUserById, registerUser } from '../controllers/userController';
 
 const router = Router();
 
 router.post('/register', registerUser);
+router.get('', getAllUsers);
+router.get('/:id', getUserById);
 
 export default router;
