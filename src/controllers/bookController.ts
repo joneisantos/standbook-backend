bb86b440cb8fc22ea7c12163634bb874ee2af852
JoneiSantos <jonei.santos@outlook.com.br>
2025-01-08 00:15:35 -0300

    routes get and update book

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -40,3 +40,86 @@ export const registerBook = async (req: Request, res: Response): Promise<void> =
     res.status(500).json({ error: 'Erro interno no servidor' });
   }
 };
+
+export const getAllBooks = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const books = await Book.find(); 
+    res.status(200).json(books);
+  } catch (error) {
+    console.error('Erro ao listar agendamentos:', error);
+    res.status(500).json({ error: 'Erro interno no servidor' });
+  }
+};
+
+export const getBookByEmployeeId = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params;
+
+    const book = await Book.find({ employeeId: id });
+    if (!book) {
+      res.status(404).json({ error: 'Agendamentos não encontrados' });
+      return;
+    }
+
+    res.status(200).json(book);
+  } catch (error) {
+    console.error('Erro ao obter agendamentos:', error);
+    res.status(500).json({ error: 'Erro interno no servidor' });
+  }
+};
+
+export const getBookByUserId = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params;
+
+    const book = await Book.find({ userId: id });
+    if (!book) {
+      res.status(404).json({ error: 'Agendamentos não encontrados' });
+      return;
+    }
+
+    res.status(200).json(book);
+  } catch (error) {
+    console.error('Erro ao obter agendamentos:', error);
+    res.status(500).json({ error: 'Erro interno no servidor' });
+  }
+};
+
+export const updateBook = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const updates = req.body;
+
+    // Validação para garantir que o ID é válido
+    if (!id) {
+      res.status(400).json({ error: 'ID do agendamento é obrigatório' });
+      return;
+    }
+ 
+    const allowedFields = ['services', 'description','status'];
+    const filteredUpdates = Object.keys(updates).reduce((obj, key) => {
+      if (allowedFields.includes(key)) {
+        obj[key] = updates[key];
+      }
+      return obj;
+    }, {} as Record<string, any>);
+
+    // Atualizar o agendamento no banco de dados
+    const updatedBook = await Book.findByIdAndUpdate(id, filteredUpdates, {
+      new: true, // Retorna o agendamento atualizado
+      runValidators: true, // Aplica validações do schema
+    });
+
+    if (!updatedBook) {
+      res.status(404).json({ error: 'Agendamento não encontrado' });
+      return;
+    }
+
+    res.status(200).json({
+      message: 'Agendamento atualizado com sucesso',
+      user: updatedBook,
+    });
+  } catch (error) {
+    console.error('Erro ao atualizar agendamento:', error);
+  }
+};
--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import { registerBook } from '../controllers/bookController';
+import { getAllBooks, getBookByEmployeeId, getBookByUserId, registerBook, updateBook } from '../controllers/bookController';
 
 const router = Router();
 
 router.post('', registerBook);
+router.get('', getAllBooks);
+router.get('/:id', getBookByEmployeeId);
+router.get('/user/:id', getBookByUserId);
+router.put('/:id', updateBook);
 
 export default router;
