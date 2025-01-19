778ecfc90d61655eb5e0a4fdf3011e300891724a
JoneiSantos <jonei.santos@outlook.com.br>
2025-01-19 01:34:45 -0300

    authentication on book-time route

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { getAllBooks, getBookByEmployeeId, getBookByUserId, registerBook, updateBook } from '../controllers/bookController';
+import { authenticate } from '../middlewares/authMiddleware';
 
 const router = Router();
 
-router.post('', registerBook);
-router.get('', getAllBooks);
-router.get('/:id', getBookByEmployeeId);
-router.get('/user/:id', getBookByUserId);
-router.put('/:id', updateBook);
+router.post('', authenticate, registerBook);
+router.get('', authenticate, getAllBooks);
+router.get('/:id', authenticate, getBookByEmployeeId);
+router.get('/user/:id', authenticate, getBookByUserId);
+router.put('/:id', authenticate, updateBook);
 
 export default router;
