4bae68651054b6625d35d204edd96c4521b5762d
JoneiSantos <jonei.santos@outlook.com.br>
2025-01-19 01:33:57 -0300

    authentication on service route

--- src/routes/serviceRoutes.ts
+++ src/routes/serviceRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { getAllservices, registerService } from '../controllers/serviceController';
+import { authenticate } from '../middlewares/authMiddleware';
 
 const router = Router();
 
-router.post('', registerService);
-router.get('', getAllservices);
+router.post('', authenticate, registerService);
+router.get('', authenticate, getAllservices);
 
 export default router;
