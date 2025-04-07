23a692ec4818af599c1891776289bb6b4a9150bb
JoneiSantos <jonei.santos@outlook.com.br>
2025-04-06 23:51:58 -0300

    removing auth in register login

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,7 +4,7 @@ import { authenticate } from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
-router.post('', authenticate, registerUser);
+router.post('', registerUser);
 router.get('', authenticate, getAllUsers);
 router.get('/:id', authenticate, getUserById);
 router.put('/:id', authenticate, updateUser);
